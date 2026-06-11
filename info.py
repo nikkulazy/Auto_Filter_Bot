bae118bc90b5894bd3d9623ad3026c01e2a316a2
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-06-12 00:31:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,8 +48,8 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001521000125'))  # Log channel i
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002016227618'))  # Bin channel id (make sure bot is admin)
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002229751777'))  # Premium logs channel id
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-100').split()] #(make sure bot is admin)
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001521000125')  # Support group id (make sure bot is admin)
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1001521000125')  # Request channel id (make sure bot is admin)
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-100')  # Support group id (make sure bot is admin)
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-100')  # Request channel id (make sure bot is admin)
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/AllnewForward_bot')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
