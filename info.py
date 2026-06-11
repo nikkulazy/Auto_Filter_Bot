1e862a46a85774994727a359aa8b677bb65d67bb
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-06-12 02:33:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,8 @@ FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c
 # Admin, Channels & Users Configuration
 # ============================
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1383239349').split()] # Replace with the actual admin ID(s) to add
-DB_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001911817898').split()]  # Channel id for auto indexing (make sure bot is admin)
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001911817898').split()]  # Channel id for auto indexing (make sure bot is admin)
+DB_CHANNEL = -1001911817898   # ← यहाँ अपनी असली चैनल ID लिखें (जहाँ फ़ाइलें स्टोर हैं)
 
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001521000125'))  # Log channel id (make sure bot is admin)
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002016227618'))  # Bin channel id (make sure bot is admin)
