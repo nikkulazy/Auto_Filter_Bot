fa3d77320c52712e11147b788704a8faebed3c32
nikkulazy <148845175+nikkulazy@users.noreply.github.com>
2026-09-08 00:23:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,8 +28,6 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")    # Bot token from @BotFather
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))    # Cache time in seconds (default: 5 minutes)
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))  # Use caption filter for search results (default: True)
 INDEX_CAPTION = bool(environ.get('SAVE_CAPTION', True)) # Save caption db when idexing make it False if you dont use USE_CAPTION_FILTER for search results (default: True)
-COVERX = is_enabled(environ.get('COVERX', "True"), True)
-ULTRA_FAST_MODE = bool(environ.get('ULTRA_FAST_MODE', True))
 #Making it false will not save caption in db SO you can save some storage space
 
 
